742d5fcc5c9fc3b11fedaf22631d2fa12a5611a5
Blaine Love <blainelove94@gmail.com>
2021-01-26 11:51:33 -0500

    signup

--- app/interface.rb
+++ app/interface.rb
@@ -1,3 +1,5 @@
+
+
 class Interface
 
     attr_reader :prompt
@@ -7,8 +9,39 @@ class Interface
     end 
 
     def welcome
-        puts 'WELCOME!'
+        puts "Welcome to Lord of the Movies"
+        prompt.select("Please select from the following") do |main|
+            main.choice "Sign in", -> { user_sign_in }
+            main.choice "Sign up", -> { user_sign_up }
+            main.choice "Exit", -> { exit }
+        end
     end 
+
+    def user_sign_in
+        # name = prompt.ask("Please enter your Username")
+        # while User.find_by(name: name)
+        #     password = prompt.ask("Please enter your Password")
+        # else 
+        #     puts "Username does not exist"
+        #     name = prompt.ask("Please enter your Username")
+        # end
+        Questionnaire.create
+    end
+
+    def user_sign_up
+        name = prompt.ask("Please enter your new Username")
+        while User.find_by(name: name)
+            puts "Account already ctreated with this name"
+            name = prompt.ask("Please enter your new Username")
+        end 
+        password = prompt.mask("Please enter your new Password")
+        User.create(name: name, password: password)
+        self.welcome
+    end
+
+    def exit
+        puts "Goodbye, Thanks for using Lord of the Movies"
+    end
 end 
 
 puts "TEST1.2.3"
